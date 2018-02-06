a8e853210fa154b0c643fa74b4371a689f01ca54
isaacmabueno <isaacmabueno@gmail.com>
2018-02-06 09:39:49 -0700

    simple loop through task array, react

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = React.createElement('h3', null, 'this is some body text');
-ReactDOM.render(element, document.getElementById('inner-inner-root'));
+const tasks = ['Take out the trash!', 'Walk the dog', 'Clean the apartment', 'Read Bible', 'Don\'t be a total douche','Keep Honor Alive', 'Hang out with Annie', 'Learn ReactJS'];
+
+// const element = React.createElement('ol', null,
+// tasks.map((task, index) => React.createElement('li', {key: index}, task))
+// );
+
+// USING JSX SYNTAX
+const element =
+<div>
+    <h1> Task List </h1>
+    <ul>
+        {tasks.map((task, index) => <li key = {index}>{task}</li>)}
+    </ul>
+</div>
+ReactDOM.render(element, document.getElementById('root'));
