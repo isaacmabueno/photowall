2a0b5495ed3e5ca0d3127de97af5e82986277ef0
isaacmabueno <isaacmabueno@gmail.com>
2018-03-01 22:58:54 -0700

    creating an img caption and remove button in the photo component

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -3,7 +3,11 @@ import React, {Component} from 'react'
 class Photo extends Component {
     render() {
         const post = this.props.post
-        return <figure> </figure>
+        return <figure className="figure">
+            <img className="photo" src={post.imageLink} alt={post.description} />
+            <figcaption><p>{post.description}</p></figcaption>
+            <button>Remove</button>
+        </figure>
     }
 }
 
--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -4,7 +4,8 @@ import Photo from './Photo'
 class PhotoWall extends Component {
     render () {
         return <div className="photoGrid">
-        {this.props.posts.map((post, index) => <Photo key= {index} post = {post}/>)}</div>
+            {this.props.posts.map((post, index) => <Photo key={index} post = {post}/>)}
+        </div>
     }
 }
 
